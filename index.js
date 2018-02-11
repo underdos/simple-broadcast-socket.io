49541f0960071569e6a8265b6039a617a0770c5c
Kusnadi <kus.underdos@gmail.com>
2018-02-11 15:42:48 +0700

    First Commit Broadcast socket.io app

---
+++ index.js
@@ -0,0 +1,35 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('broadcast', function(msg){
+    io.emit('broadcast', msg);
+    console.log('message: ' + msg);
+  });
+});
+
+
+i = 0;
+setInterval(function(){
+ io.emit('counter', 'Counter '+i);
+  console.log('Counter '+i);
+  i++;
+}, 5000);  
+
+var response = { status: 200, message: 'Success', response: 'Helloooowww' };
+setInterval(function(){
+ io.emit('json', response);
+}, 5000);
+
+
+
+http.listen(80, function(){
+  console.log('listening on *:80');
+});
+
